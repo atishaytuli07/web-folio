ec8150ef0129d3a712705a5c3101ea18d945a13e
atishaytuli07 <atishaytuliiaf@gamil.com>
2026-03-15 22:37:52 +0530

    hello v2

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-document.querySelectorAll('.flip-container').forEach(container => {
-    container.addEventListener('click', () => {
-        container.classList.toggle('clicked');
-    });
-});
+// portfolio script
